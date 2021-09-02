7d2088ece4b55ddd1e525a816c206339e150507f
Pannu786 <thepannu786@gmail.com>
2021-09-02 22:18:17 +0530

    All dne but need to fix server communication

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -9,16 +9,17 @@ import {
 } from 'react-admin';
 
 const PostList = (props) => {
-  return <List {...props}>
-    <Datagrid>
-      <TextField source='id' />
-      <TextField source='title' />
-      <DateField source='publishedAt' />
-      <EditButton basePath='/posts' />
-      <DeleteButton basePath='/posts' />
-
-    </Datagrid>
-  </List>;
+  return (
+    <List {...props}>
+      <Datagrid>
+        <TextField source='id' />
+        <TextField source='title' />
+        <DateField source='publishedAt' />
+        <EditButton basePath='/posts' />
+        <DeleteButton basePath='/posts' />
+      </Datagrid>
+    </List>
+  );
 };
 
 export default PostList;
